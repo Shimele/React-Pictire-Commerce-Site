b01360951a7d03b63e91382d5f7bd78e76abe47f
shimele <bechembayamba@gmail.com>
2021-03-18 20:39:57 +0100

    place order

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -1,14 +1,20 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import "../styles.css";
 import { Context } from "../components/Context";
 
 function CartItem({ item }) {
+  const [hovered, setHovered] = useState(false);
   const { removeFromCart } = useContext(Context);
+
+  const deleteIcon = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
+
   return (
     <div className="cart-item">
       <i
-        className="ri-delete-bin-line"
+        className={deleteIcon}
         onClick={() => removeFromCart(item.id)}
+        onMouseEnter={() => setHovered(true)}
+        onMouseEnter={() => setHovered(false)}
       ></i>
       <img src={item.url} width="130px" />
       <p>$5.99</p>
